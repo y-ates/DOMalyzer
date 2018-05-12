752782a34de3d66cec6817790ba35fe4b8efcc14
Yakup Ates <Yakup.Ates@rub.de>
2018-05-12 20:24:21 +0200

    create dom in DOMgenerator

--- index.php
+++ index.php
@@ -19,9 +19,10 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-include '0_control/DOMbuilder.php';
+//include '0_control/DOMbuilder.php';
+include '0_model/DOMgenerator.php';
 
-
+/*
 $builder = new DOMbuilder();
 
 $form = $builder->create_element("form", "body");
@@ -31,5 +32,9 @@ $input = $builder->create_element("input", "form");
 $builder->add_attribute($input, "name", "attributes");
 
 echo $builder->get_HTML();
+*/
+
+$generator = new DOMgenerator();
+
 
 ?>
