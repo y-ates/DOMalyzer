9f8673e059ca5e5c4071e410e7b2d337ff4765d3
Yakup Ates <Yakup.Ates@rub.de>
2018-06-05 03:39:53 +0200

    write to file using php

---
+++ filewriter.php
@@ -0,0 +1,26 @@
+<?php
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+
+$filename = "test.txt";
+
+$content = file_put_contents($filename, file_get_contents('php://input') . "\r\n", FILE_APPEND);
+
+
+echo $content;
+?>
